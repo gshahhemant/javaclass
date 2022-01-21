5895ecd785beb0b8bf86e9b0cd084c54d474cf9a
gshahhemant <33495488+gshahhemant@users.noreply.github.com>
2022-01-21 13:07:40 -0600

    Add files via upload

---
+++ com.loop/SimpleWhileLoop.java
@@ -0,0 +1,21 @@
+package com.loop;
+
+public class SimpleWhileLoop {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+
+		int i = 1;
+
+		while (i <= num1) {
+
+			System.out.println(i);
+
+			i = i + 1;
+
+		}
+
+	}
+
+}
---
+++ com.loop/SumOfDigit.java
@@ -0,0 +1,35 @@
+package com.loop;
+
+public class SumOfDigit {
+
+	public static void main(String[] args) {
+
+		int num1 = 56;
+
+		int i = 1;
+
+		int sum = 0;
+
+		while (i <= num1) {
+
+			System.out.print(i + "+");
+
+			sum = sum + i;
+
+			i = i + 1;
+
+		}
+		
+		
+		System.out.println("=  " + sum);
+
+	}
+
+}
+
+
+/*
+ 
+ 
+ * 
+ */
