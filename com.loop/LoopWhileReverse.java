1453d63fd7463944e1c90a29e056c38167b058da
gshahhemant <33495488+gshahhemant@users.noreply.github.com>
2022-02-12 14:35:44 -0600

    Add files via upload

---
+++ com.loop/LoopWhileReverse.java
@@ -0,0 +1,18 @@
+package com.loop;
+
+public class LoopWhileReverse {
+
+	public static void main(String args[]) {
+
+		int num1 = 10;
+
+		while (num1 > 0) {
+			
+			System.out.println(num1);
+
+			num1 = num1 - 1;
+
+		}
+
+	}
+}
---
+++ com.loop/SimpleWhileFactorial.java
@@ -0,0 +1,20 @@
+package com.loop;
+
+public class SimpleWhileFactorial {
+
+	public static void main(String[] args) {
+
+		int num1 = 5;
+		int i = 1;
+
+		while (i <= num1) {
+
+			System.out.println(i);
+
+			i = i + 1;
+
+		}
+
+	}
+
+}
