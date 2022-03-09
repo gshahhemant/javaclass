271aea2ab7a53b7ced6e89e0fa703a133196c0ae
gshahhemant <33495488+gshahhemant@users.noreply.github.com>
2022-03-09 17:54:38 -0600

    Add files via upload

---
+++ com.objectoriented/Student.java
@@ -0,0 +1,17 @@
+package com.objectoriented;
+
+public class Student {
+	
+	
+	/*  Below are all properties of student  */
+	
+	public int studentId;
+	public String studentName;
+	public String teacherName;
+	public String address;
+	public String password;
+	public int emContact;
+	
+	
+
+}
