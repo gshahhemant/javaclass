48c8c45728bd5f7122d9b9810ed4171ee7cebd9d
gshahhemant <33495488+gshahhemant@users.noreply.github.com>
2022-04-13 18:59:28 -0500

    Add files via upload

---
+++ com.library/MyLibraryTest.java
@@ -0,0 +1,19 @@
+package com.library;
+
+public class MyLibraryTest {
+
+	public static void main(String[] args) {
+
+		MyLibrary m = new MyLibrary();
+
+		System.out.println(m.largestTwoNum(2000, 5000));
+		
+		System.out.println(m.isEven(200));
+		
+		System.out.println(m.isOdd(200));
+		
+		System.out.println(m.sumOfDigit(10));
+
+	}
+
+}
