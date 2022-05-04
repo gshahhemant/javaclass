0e337f04b810fc98ac1ef96abfec291575f2a5b1
gshahhemant <33495488+gshahhemant@users.noreply.github.com>
2022-05-04 17:36:59 -0500

    Update CollectionList.java

--- com.collection/CollectionList.java
+++ com.collection/CollectionList.java
@@ -5,49 +5,54 @@ import java.util.ArrayList;
 public class CollectionList {
 
 	public static void main(String[] args) {
-		
+
 		// int , long float double are primitive data types
 
-		ArrayList<String> lst = new ArrayList<String>(); // allocate the memory
+		ArrayList<String> lst = new ArrayList<String>(); // Creating object of ArrayList . New allocate the memory
+															// lst is object name
 
 		// ArrayList is class created by java
 		// here we have object not variable
 
-		lst.add("hemant");
-		lst.add("Shaurya");
-		lst.add("Vikhyat");
-		lst.add("tatva");
-		
-		lst.add("hemant1");
-		lst.add("Shaurya2");
-		lst.add("Vikhyat4");
-		lst.add("tatva");
-		
-		//lst.remove("tatva");
-		
-	//	lst.clear();
+		lst.add("OM");
+		lst.add("Ansh");
+		lst.add("Aayushi");
+		lst.add("Samay");
+		lst.add("Samay");
+		lst.add("Samay");
+		lst.add("Samay");
+
+		// lst.remove(0);
+		// lst.removeAll(lst); same as clear
+
+		// lst.clear(); clear all list elements
 
 		for (String p : lst) {
+
 			System.out.println(p);
 		}
 
-			
-		ArrayList<Integer> lstInt = new ArrayList<Integer>(); // allocate the memory
-
-		// ArrayList is class created by java
-		// here we have object not variable
+		
+		
+		ArrayList<Integer> lstInt = new ArrayList<Integer>(); 
+		// int will not work we need to write Integer because ArrayList need Class. 
+		//Integer is class and int is primitive data types.
+		
 
 		lstInt.add(1);
 		lstInt.add(2);
 		lstInt.add(3);
 		lstInt.add(4);
 
-		for (Integer p1: lstInt) {
-			System.out.println(p1);
+		for (Integer intdata : lstInt) {
+			
+			System.out.println(intdata);
 		}
+		
+		
+		
+		
 
-		
-		
 	}
 
 }
